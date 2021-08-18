d723160c5b2f1b12a5a9bbcb1a5812e7d2edc0b3
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2021-08-18 18:24:41 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//Correção Questão 7
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "Time.h"
+
+//o uso de mÃ©todos inline nÃ£o alteram a lÃ³gica do programa
+
+int main() {
+  Time t;
+
+  t.printUniversal();
+
+  return 0;
+}
